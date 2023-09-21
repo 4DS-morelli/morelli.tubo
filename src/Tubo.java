b87710a218a6dbdc3f04189e6045c5bf36cb3c83
nicola.morelli <nicola.morelli@buonarroti.tn.it>
2023-09-21 14:31:12 +0200

    feat: aggiunto metodo costruttore classe tubo

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,4 +1,9 @@
 public class Tubo {
     private int altezza;
     private int raggio;
+
+    public Tubo(int altezza, int raggio) {
+        this.altezza = altezza;
+        this.raggio = raggio;
+    }
 }
