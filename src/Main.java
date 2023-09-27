3bb810aeb7146a36a9212d376aa8e5e1c4caa843
nicola.morelli <nicola.morelli@buonarroti.tn.it>
2023-09-27 16:41:56 +0200

    test: aggiunte variabili altezza e raggio e corripsettivi input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner input= new Scanner(System.in);
+
+        int altezza, raggio;
+        System.out.print("Inserisci il raggio: ");
+        raggio=input.nextInt();
+        System.out.print("Inserisci l'altezza: ");
+        altezza=input.nextInt();
+
     }
 }
