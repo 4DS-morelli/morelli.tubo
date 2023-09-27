e91ce252297bd478e4206802876ef02cab8eaae8
nicola.morelli <nicola.morelli@buonarroti.tn.it>
2023-09-27 16:44:58 +0200

    test: aggiunta variabile di tipo Tubo e uso dei metodi volume e superficieTot nel main

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,19 @@ public class Main {
         Scanner input= new Scanner(System.in);
 
         int altezza, raggio;
+        double volume, superficie;
         System.out.print("Inserisci il raggio: ");
         raggio=input.nextInt();
         System.out.print("Inserisci l'altezza: ");
         altezza=input.nextInt();
+
+        Tubo t1= new Tubo(altezza, raggio);
+
+        volume= t1.volume();
+        superficie=t1.superficieTot();
+
+        System.out.print("Il volume del tubo è: " + volume + " e la superficie totale del tubo è: " + superficie);
+
 
     }
 }
